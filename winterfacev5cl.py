e8d10783e5e4bcf38ed662acb42527465b101c04
darkolol9 <53996212+darkolol9@users.noreply.github.com>
2020-05-02 14:31:54 +0300

    h

--- winterfacev5cl.py
+++ winterfacev5cl.py
@@ -121,7 +121,7 @@ while 1:
 			line =  '[' + winterface[0] + '] ' + '[' +  winterface[1]+ '] ' + '[' + winterface[2] + '] ' +'[' + winterface[3]+ ']'
 		blank_line = True
 
-		if "floor" in floor:
+		if "Floor" in floor:
 			blank_line = False
 			print('successfully captured a floor winterface!')
 			print(winterface)
